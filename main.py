afc2d7d1313b27a116a694d513221b370d38d354
Eng Sung <engxsung@gmail.com>
2021-11-11 23:17:38 -0500

    getting to the result table

--- main.py
+++ main.py
@@ -17,10 +17,26 @@ def main():
         p = s.post("https://sqct.uwo.ca/results/login.cfm", data=payload)
         # Navigate to the next page and scrape the data
 
-        # An authorised request.
-        r = s.get('https://sqct.uwo.ca/results/search.cfm')
-        soup = BeautifulSoup(r.content, 'html.parser')
+        # An authorised request
+        p = s.get('https://sqct.uwo.ca/results/search.cfm')
+        option_payload = {
+            'InsFirstName': '',
+            'InsLastName': '',
+            'teachingFaculty': '',
+            'teachingDept': '',
+            'courseSubject': '',
+            'courseNumber': '',
+            'classSec': '',
+            'acadYear': '2019 Fall/Winter',
+            'acadTerm': None
+        }
+        p = s.post("https://sqct.uwo.ca/results/results.cfm", data=option_payload)
+
+        print(p.text)
+
+        soup = BeautifulSoup(p.content, 'html.parser')
         print(soup.title.text)
+        print(soup.find('h4').text)
 
 
 if __name__ == '__main__':
