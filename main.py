12608e71324847b72941eae79f1387e333c8ccea
Eng Sung <engxsung@gmail.com>
2021-11-12 01:47:50 -0500

    finished

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     f.write("Instructor,Faculty,Mean Displays Enthusiasm,Mean Well Organized,Mean Presents Concepts Clearly,"
             "Mean Encourages Participation,Mean Responds To Questions Clearly,Mean Encourages Reflection,"
             "Mean Provides Fair Evaluation,Mean Provides Helpful Feedback,Mean Good Motivator,Mean Average 1 to 9,"
-            "Mean Effective As A University Teacher,Mean Course As Learning Experience\n")
+            "Mean Effective As A University Teacher,Mean Course As Learning Experience,Department,Subject,Course\n")
 
     # Create the payload
     load_dotenv()
@@ -44,22 +44,23 @@ def main():
         for table in tables:
             # TODO: ERASE THE 10
             for tr in table.findAll("tr")[1:10]:
-                for td in tr.findAll("td"):
-                    for a in td.findAll("a"):
-                        prof_url = 'https://sqct.uwo.ca/results/'+a["href"]
-                        prof = s.get(prof_url)
-                        prof_soup = BeautifulSoup(prof.content, 'html.parser')
-                        tables = prof_soup.findAll("table")
+                tds = tr.findAll("td")
+                for a in tds[0].findAll("a"):
+                    prof_url = 'https://sqct.uwo.ca/results/'+a["href"]
+                    prof = s.get(prof_url)
+                    prof_soup = BeautifulSoup(prof.content, 'html.parser')
+                    tables = prof_soup.findAll("table")
 
-                        table1_tr = tables[0].findAll("tr")[1]
-                        table1_td = table1_tr.findAll("td")
-                        f.write(table1_td[0].text+','+table1_td[1].text+',')
+                    table1_tr = tables[0].findAll("tr")[1]
+                    table1_td = table1_tr.findAll("td")
+                    f.write(table1_td[0].text+','+table1_td[1].text+',')
 
-                        table2_tr = tables[1].findAll("tr")[1:]
-                        for tr in table2_tr:
-                            inside_table2_td = tr.findAll("td")
-                            f.write(inside_table2_td[9].text.strip()+',')
-                        f.write('\n')
+                    table2_tr = tables[1].findAll("tr")[1:]
+                    for tr in table2_tr:
+                        inside_table2_td = tr.findAll("td")
+                        f.write(inside_table2_td[9].text.strip()+',')
+                f.write(tds[3].text+','+tds[4].text+','+tds[5].text+'\n')
+                # f.write(tds[3].text + ',' + tds[4].text + ',' + tds[5].text + '\n')
 
         print("Finished extracting data")
 
